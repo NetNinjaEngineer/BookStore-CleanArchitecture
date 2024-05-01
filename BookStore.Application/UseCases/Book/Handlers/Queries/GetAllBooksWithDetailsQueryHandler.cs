994eef366d822ba7fec3b0e8815601ea5bb3d52c
NetNinjaEngineer <memo5260287@outlook.com>
2024-05-01 16:07:55 +0300

    Refactor GetAllBooksWithDetailsQueryHandler

--- BookStore.Application/UseCases/Book/Handlers/Queries/GetAllBooksWithDetailsQueryHandler.cs
+++ BookStore.Application/UseCases/Book/Handlers/Queries/GetAllBooksWithDetailsQueryHandler.cs
@@ -7,16 +7,18 @@ using Microsoft.AspNetCore.Http;
 namespace BookStore.Application.UseCases.Book.Handlers.Queries;
 public sealed class GetAllBooksWithDetailsQueryHandler(
     IUnitOfWork unitOfWork,
-    IHttpContextAccessor contextAccessor)
-        : IRequestHandler<GetAllBooksWithDetailsQuery,
-            IQueryable<BookWithDetailsDto>>
+    IHttpContextAccessor? contextAccessor)
+        : IRequestHandler<GetAllBooksWithDetailsQuery, IQueryable<BookWithDetailsDto>>
 {
     public Task<IQueryable<BookWithDetailsDto>> Handle(
         GetAllBooksWithDetailsQuery request,
-        CancellationToken cancellationToken)
+        CancellationToken cancellationToken
+        )
     {
-        var httpRequest = contextAccessor.HttpContext.Request;
-        var baseUrl = $"{httpRequest.Scheme}://{httpRequest.Host}{httpRequest.PathBase}";
+        var httpRequest = (contextAccessor?.HttpContext?.Request)
+            ?? throw new InvalidOperationException("HTTP context is not available.");
+
+        var baseUrl = $"{httpRequest.Scheme}://{httpRequest.Host}";
 
         var booksWithDetails = unitOfWork.BookRepository
             .FindAll(book => book.Genre, book => book.Authors)
@@ -28,10 +30,13 @@ public sealed class GetAllBooksWithDetailsQueryHandler(
                 PublicationYear = book.PublicationYear,
                 Genre = book.Genre.GenreName,
                 Authors = book.Authors.Select(x => x.AuthorName),
-                ImageUrl = Path.Combine(baseUrl, $"Files/Images/Books/{book.ImageName}")
+                ImageUrl = GetImageUrl(book, baseUrl)
             });
 
         return Task.FromResult(booksWithDetails);
 
     }
+
+    private static string GetImageUrl(Domain.Book book, string baseUrl)
+        => Path.Combine(baseUrl, $"Files/Images/Books/{book.ImageName}");
 }
