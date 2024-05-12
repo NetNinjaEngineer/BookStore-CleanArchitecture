448b77a462cd63a0648e8dd5810dfab8e843880c
NetninjaEngineer <memo5260287@outlook.com>
2024-05-13 00:33:51 +0300

    Refactor GetAllBooksWithDetailsQueryHandler to work with mappings

--- BookStore.Application/UseCases/Book/Handlers/Queries/GetAllBooksWithDetailsQueryHandler.cs
+++ BookStore.Application/UseCases/Book/Handlers/Queries/GetAllBooksWithDetailsQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using BookStore.Application.Contracts.Infrastructure;
+﻿using AutoMapper;
+using BookStore.Application.Contracts.Infrastructure;
 using BookStore.Application.Dtos.Book;
 using BookStore.Application.Specifications.Features.Book;
 using BookStore.Application.UseCases.Book.Requests.Queries;
@@ -6,29 +7,14 @@ using MediatR;
 
 namespace BookStore.Application.UseCases.Book.Handlers.Queries;
 public sealed class GetAllBooksWithDetailsQueryHandler(
-    IUnitOfWork unitOfWork)
+    IUnitOfWork unitOfWork,
+    IMapper mapper)
         : IRequestHandler<GetAllBooksWithDetailsQuery, IEnumerable<BookForListDto>>
 {
     public async Task<IEnumerable<BookForListDto>> Handle(
         GetAllBooksWithDetailsQuery request,
         CancellationToken cancellationToken
         )
-    {
-
-        var query = await unitOfWork
-            .BookRepository
-            .GetAllWithSpecifications(new GetAllBooksWithGenreAndAuthorsSpecification());
-
-        return query.Select(x => new BookForListDto
-        {
-            Id = x.Id,
-            Title = x.Title,
-            Price = x.Price,
-            PublicationYear = x.PublicationYear,
-            Genre = x.Genre.GenreName,
-            Authors = x.Authors.Select(x => x.AuthorName),
-            ImageUrl = Utility.Utility.GetImageUrl(x.ImageName!),
-            ImageName = x.ImageName
-        });
-    }
+        => mapper.Map<IEnumerable<BookForListDto>>(
+            await unitOfWork.BookRepository.GetAllWithSpecifications(new GetAllBooksWithGenreAndAuthorsSpecification()));
 }
